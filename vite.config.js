af5097714722ac54f70dcc0cba6ef726720f4678
Madhu Sharma <madhusharmamain7@gmail.com>
2025-03-19 22:12:31 +0545

    Fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/meme-generator/', // 👈 This is important for GitHub Pages
+  base: "/meme-generator/", // 👈 Must exactly match your repo name
 });
