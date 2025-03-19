0064a2dbd0e0758ded6343871c6dcf0fbf3f909b
Madhu Sharma <madhusharmamain7@gmail.com>
2025-03-19 22:03:00 +0545

    vite config file changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/meme-generator/', // 👈 This is important for GitHub Pages
+});
